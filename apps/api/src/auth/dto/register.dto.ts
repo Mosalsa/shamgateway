f46ffc62ac81be9de9eed8494cec33d465b2b974
Mosalsa <ouday.alyah.sahra@gmail.com>
2025-06-30 03:04:28 +0200

    api-user

--- apps/api/src/auth/dto/register.dto.ts
+++ apps/api/src/auth/dto/register.dto.ts
@@ -1,5 +1,7 @@
 import { IsEmail, IsOptional, MinLength } from "class-validator";
-
+import { PartialType } from "@nestjs/mapped-types";
+import { CreateUserDto } from "./create-user.dto";
+export class UpdateUserDto extends PartialType(CreateUserDto) {}
 export class RegisterDto {
   @IsEmail()
   email!: string;
